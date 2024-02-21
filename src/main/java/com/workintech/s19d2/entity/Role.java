90d4774db6f24850dfcee4a594ee285845ba63b9
ruhatb <r.uhat@yahoo.com.tr>
2024-02-21 21:40:14 +0300

    done Security system..

---
+++ src/main/java/com/workintech/s19d2/entity/Role.java
@@ -0,0 +1,21 @@
+package com.workintech.s19d2.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "role", schema = "bank")
+public class Role implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "authority")
+    private String authority;
+}
